a5b74329d8e9d5c4bfdcdd40a6e1664396af8b6e
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-02-13 20:25:51 +0700

    Create Persisting merged DataFrame.py

---
+++ 5.Delaying reading & cleaning/Persisting merged DataFrame.py
@@ -0,0 +1,14 @@
+# Print time in milliseconds to compute percent_delayed on weather_delays
+t_start = time.time()
+print(percent_delayed(weather_delays).compute())
+t_end = time.time()
+print((t_end-t_start)*1000)
+
+# Call weather_delays.persist(): persisted_weather_delays
+persisted_weather_delays =  weather_delays.persist()
+
+# Print time in milliseconds to compute percent_delayed on persisted_weather_delays
+t_start = time.time()
+print(percent_delayed(persisted_weather_delays).compute())
+t_end = time.time()
+print((t_end-t_start)*1000)
